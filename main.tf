5fbae1fa0ac30c1aeb34f9e2bf9be2a068788370
smotla <143429599+smotla@users.noreply.github.com>
2023-08-28 15:41:13 -0500

    fix errors for module

--- main.tf
+++ main.tf
@@ -18,21 +18,20 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_group_ids = [module.blog_sg_module.security_group_id]
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
     Name = "HelloWorld"
   }
 }
 
-module "blog_sg_module" {
+module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.1.0"
 
   vpc_id = data.aws_vpc.default.id
-  name                = "blog"
-
-  ingress_rules       = ["http-80-tcp", "https-443-80"]
+  name   = "blog"
+  ingress_rules       = ["https-443-80","http-80-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
   egress_rules        = ["all-all"]
   egress_cidr_blocks  = ["0.0.0.0/0"]
