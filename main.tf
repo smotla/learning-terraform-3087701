fa0554d3437231973028e2dbd346f2cba80f65bc
smotla <143429599+smotla@users.noreply.github.com>
2023-08-29 13:22:58 -0500

    vpc module added

--- main.tf
+++ main.tf
@@ -14,6 +14,24 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
+module "blog_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
+
+
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
@@ -29,7 +47,7 @@ module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.1.0"
 
-  vpc_id = data.aws_vpc.default.id
+  vpc_id = module.vpc.public_subnets[0]
   name   = "blog_new"
   ingress_rules       = ["https-443-80","http-80-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
@@ -37,6 +55,3 @@ module "blog_sg" {
   egress_cidr_blocks  = ["0.0.0.0/0"]
 }
 
-data "aws_vpc" "default" {
-  default = true
-}
